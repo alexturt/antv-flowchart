fec9887a19a31e9e9f33c3a5e896a556c979626f
hophiphip <nighop@yandex.ru>
2023-05-13 00:51:22 +0400

    Using flowchart instead of xflow

--- src/App.tsx
+++ src/App.tsx
@@ -1,185 +1,227 @@
-import type { IAppLoad } from '@antv/xflow'
-
-import React, { useRef, useEffect } from 'react'
-/** 交互组件 */
-import {
-    /** XFlow核心组件 */
-        XFlow,
-    /** 流程图画布组件 */
-        FlowchartCanvas,
-    /** 流程图配置扩展 */
-        FlowchartExtension,
-    /** 流程图节点组件 */
-        FlowchartNodePanel,
-    /** 流程图表单组件 */
-        FlowchartFormPanel,
-    /** 通用组件：快捷键 */
-        KeyBindings,
-    /** 通用组件：画布缩放 */
-        CanvasScaleToolbar,
-    /** 通用组件：右键菜单 */
-        CanvasContextMenu,
-    /** 通用组件：工具栏 */
-        CanvasToolbar,
-    /** 通用组件：对齐线 */
-        CanvasSnapline,
-    /** 通用组件：节点连接桩 */
-        CanvasNodePortTooltip,
-} from '@antv/xflow'
-/** 配置Command*/
-import { useCmdConfig } from './config-cmd'
-/** 配置Menu */
-import { useMenuConfig } from './config-menu'
-/** 配置Toolbar */
-import { useToolbarConfig } from './config-toolbar'
-/** 配置快捷键 */
-import { useKeybindingConfig } from './config-keybinding'
-//import type { Node, Edge } from '@antv/x6';
-import { Graph } from '@antv/x6';
+import { Suspense, lazy } from "react";
 import "antd4/dist/antd.css";
 import "@ant-design/flowchart/dist/index.css";
-import './index.less'
-const DndNode = (props) => {
-    const { size = { width: 126, height: 104 }, data } = props
-    const { width, height } = size
-    const { label, stroke, fill, fontFill, fontSize } = data
+import "./index.less";
 
-    return (
-        <div
-            className="container"
-            style={{
-                width,
-                height,
-                borderColor: stroke,
-                backgroundColor: fill,
-                color: fontFill,
-                fontSize,
-            }}
-        >
-            <span>{label}</span>
-        </div>
-    )
-}
-export interface IProps {
-    meta: { flowId: string }
-}
+const Flowchart = lazy(() =>
+	import("@ant-design/flowchart").then((module) => ({
+		default: module.Flowchart,
+	})),
+);
 
-export const Demo: React.FC<IProps> = props => {
-    const { meta } = props
-    const toolbarConfig = useToolbarConfig()
-    const menuConfig = useMenuConfig()
-    const keybindingConfig = useKeybindingConfig()
-    const graphRef = useRef<Graph>()
-    const commandConfig = useCmdConfig()
-    /**
-     * @param app 当前XFlow工作空间
-     * @param extensionRegistry 当前XFlow配置项
-     */
-
-    const onLoad: IAppLoad = async app => {
-        graphRef.current = await app.getGraphInstance()
-        graphRef.current
-        const source = graphRef.current?.addNode({
-            shape: 'custom-node-width-port',
-            x: 40,
-            y: 40,
-            label: 'hello',
-            ports: {
-                items: [
-                    {
-                        id: 'port_1',
-                        group: 'bottom',
-                    },
-                    {
-                        id: 'port_2',
-                        group: 'bottom',
-                    },
-                ],
-            },
-        })
-        const target = graphRef.current?.addNode({
-            shape: 'custom-node-width-port',
-            x: 160,
-            y: 180,
-            label: 'world',
-            ports: {
-                items: [
-                    {
-                        id: 'port_3',
-                        group: 'top',
-                    },
-                    {
-                        id: 'port_4',
-                        group: 'top',
-                    },
-                ],
-            },
-        })
-        console.log(graphRef)
-        console.log(source)
-        console.log(target)
-    }
-
-    useEffect(() => {
-        if (graphRef.current) {
-            graphRef.current.on('node:click', (...arg) => {
-                //console.log(arg)
-            })
-        }
-    }, [graphRef])
-
-
-    return (
-        <XFlow
-            className="flow-user-custom-clz"
-            commandConfig={commandConfig}
-            onLoad={onLoad}
-            meta={meta}
-        >
-            <FlowchartExtension />
-            <FlowchartNodePanel
-                registerNode={{
-                    title: 'custom', key: '1',
-                    nodes: [
-                        {
-                            component: DndNode,
-                            popover: () => <div>自定义节点</div>,
-                            name: 'custom-node-width-port',
-                            width: 210,
-                            height: 130,
-                            label: 'custom-node-width-port',
-                        },
-                    ],
-                }}
-                position={{ width: 162, top: 40, bottom: 0, left: 0 }}
-            />
-            <CanvasToolbar
-                className="xflow-workspace-toolbar-top"
-                layout="horizontal"
-                config={toolbarConfig}
-                position={{ top: 0, left: 0, right: 0, bottom: 0 }}
-            />
-            <FlowchartCanvas position={{ top: 40, left: 0, right: 0, bottom: 0 }}>
-                <CanvasScaleToolbar
-                    layout="horizontal"
-                    position={{ top: -40, right: 0 }}
-                    style={{
-                        width: 150,
-                        left: 'auto',
-                        height: 39,
-                    }}
-                />
-                <CanvasContextMenu config={menuConfig} />
-                <CanvasSnapline color="#faad14" />
-                <CanvasNodePortTooltip />
-            </FlowchartCanvas>
-            <FlowchartFormPanel show={true} position={{ width: 200, top: 40, bottom: 0, right: 0 }} />
-            <KeyBindings config={keybindingConfig} />
-        </XFlow>
-    )
-}
+const magnetAvailabilityHighlighter = {
+	name: "stroke",
+	args: {
+		padding: 3,
+		attrs: {
+			strokeWidth: 3,
+			stroke: "#52c41a",
+		},
+	},
+};
 
 function App() {
-    return <Demo meta={{ flowId: 'string' }} />;
+	return (
+		<Suspense fallback={<div style={{ height: '100%', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>I am loading... (╯°□°)╯</div>}>
+			<Flowchart
+				canvasProps={{
+					config: {
+						grid: true,
+						highlighting: {
+							magnetAvailable: magnetAvailabilityHighlighter,
+						},
+						connecting: {
+							snap: true,
+							allowBlank: false,
+							allowLoop: false,
+							allowNode: false,
+							highlight: true,
+							validateMagnet({ magnet }) {
+								return magnet.getAttribute("port-group") !== "in";
+							},
+
+							validateConnection({ sourceMagnet, targetMagnet }) {
+								if (
+									!sourceMagnet ||
+									sourceMagnet.getAttribute("port-group") === "in"
+								) {
+									return false;
+								}
+
+								if (
+									!targetMagnet ||
+									targetMagnet.getAttribute("port-group") !== "in"
+								) {
+									return false;
+								}
+
+								return true;
+							},
+						},
+					},
+				}}
+				onReady={(graph) => {
+					const source = graph.addNode({
+						x: 40,
+						y: 40,
+						width: 100,
+						height: 40,
+						attrs: {
+							body: {
+								fill: "#f5f5f5",
+								stroke: "#d9d9d9",
+								strokeWidth: 1,
+							},
+						},
+						ports: {
+							groups: {
+								in: {
+									position: { name: "top" },
+								},
+								out: {
+									position: { name: "bottom" },
+								},
+							},
+							items: [
+								{ id: "in-1", group: "in" },
+								{ id: "in-2", group: "in" },
+								{ id: "out-1", group: "out" },
+								{ id: "out-2", group: "out" },
+							],
+						},
+						portMarkup: [
+							{
+								tagName: "circle",
+								selector: "portBody",
+								attrs: {
+									r: 5,
+									magnet: 1,
+									stroke: "#31d0c6",
+									fill: "#fff",
+									strokeWidth: 2,
+								},
+							},
+						],
+					});
+
+					const target = graph.addNode({
+						x: 140,
+						y: 240,
+						width: 100,
+						height: 40,
+						attrs: {
+							body: {
+								fill: "#f5f5f5",
+								stroke: "#d9d9d9",
+								strokeWidth: 1,
+							},
+						},
+						ports: {
+							groups: {
+								in: {
+									position: { name: "top" },
+								},
+								out: {
+									position: { name: "bottom" },
+								},
+							},
+							items: [
+								{ id: "in-1", group: "in" },
+								{ id: "in-2", group: "in" },
+								{ id: "out-1", group: "out" },
+								{ id: "out-2", group: "out" },
+							],
+						},
+						portMarkup: [
+							{
+								tagName: "circle",
+								selector: "portBody",
+								attrs: {
+									r: 5,
+									magnet: 1,
+									stroke: "#31d0c6",
+									fill: "#fff",
+									strokeWidth: 2,
+								},
+							},
+						],
+					});
+
+					graph.addNode({
+						x: 320,
+						y: 120,
+						width: 100,
+						height: 40,
+						attrs: {
+							body: {
+								fill: "#f5f5f5",
+								stroke: "#d9d9d9",
+								strokeWidth: 1,
+							},
+						},
+						ports: {
+							groups: {
+								in: {
+									position: { name: "top" },
+								},
+								out: {
+									position: { name: "bottom" },
+								},
+							},
+							items: [
+								{ id: "in-1", group: "in" },
+								{ id: "in-2", group: "in" },
+								{ id: "out-1", group: "out" },
+								{ id: "out-2", group: "out" },
+							],
+						},
+						portMarkup: [
+							{
+								tagName: "circle",
+								selector: "portBody",
+								attrs: {
+									r: 5,
+									magnet: 1,
+									stroke: "#31d0c6",
+									fill: "#fff",
+									strokeWidth: 2,
+								},
+							},
+						],
+					});
+
+					graph.addEdge({
+						source: { cell: source.id, port: "out-2" },
+						target: { cell: target.id, port: "in-1" },
+					});
+
+					graph.addEdge({
+						source: { cell: source.id, port: "out-1" },
+						target: { cell: target.id, port: "in-2" },
+					});
+
+					graph.on("edge:mouseenter", ({ cell }) => {
+						cell.addTools([
+							{
+								name: "source-arrowhead",
+							},
+							{
+								name: "target-arrowhead",
+								args: {
+									attrs: {
+										fill: "red",
+									},
+								},
+							},
+						]);
+					});
+
+					graph.on("edge:mouseleave", ({ cell }) => {
+						cell.removeTools();
+					});
+				}}
+			/>
+		</Suspense>
+	);
 }
 export default App;
